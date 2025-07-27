e35e72f942de447b092d7c9f54ea391d58d967cc
yutaootsubo <ootsubo.yuta@gmail.com>
2025-07-27 13:56:54 +0900

    Counter

--- test_react_next.js_week5/app/page.js
+++ test_react_next.js_week5/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Counter from "./Counter";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Counter />
       <main className={styles.main}>
         <Image
           className={styles.logo}
